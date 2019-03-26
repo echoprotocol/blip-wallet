9b98613a0e85a3bf2376f4d0c81223124ab9378e
Andrey Fedorovich <a.fedorovich@pixelplex.io>
2019-03-26 17:30:39 +0300

    [BLP-33] css choose language

---
+++ app/containers/select-language/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { Button, Icon } from 'semantic-ui-react';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+
+import blipLogo from '../../assets/images/blip-logo.svg';
+
+class SelectLanguage extends React.Component {
+
+	render() {
+
+		return (
+			<div className="main-bg">
+				<a href="/" className="logo-wrap">
+					<img src={blipLogo} alt="" />
+
+				</a>
+				<div className="lang-wrap">
+					<h3>Choose your preferred language</h3>
+
+					<Dropdown
+						className="dropdown-lang"
+					>
+						<div className="dropdown-label">
+							{'	language'}
+						</div>
+
+						<Dropdown.Toggle variant="Info">
+							<div className="lang-title">English</div>
+							<span className="carret" />
+						</Dropdown.Toggle>
+
+						<Dropdown.Menu>
+							<PerfectScrollbar className="lang-scroll">
+								<Dropdown.Item
+									eventKey="1"
+								>
+									{'English'}
+								</Dropdown.Item>
+								<Dropdown.Item eventKey="2">
+									{'Русский (Russian)'}
+								</Dropdown.Item>
+								<Dropdown.Item eventKey="3">
+									{'Deutsch (German)'}
+								</Dropdown.Item>
+							</PerfectScrollbar>
+						</Dropdown.Menu>
+					</Dropdown>
+
+					<Button
+						className="btn-primary arrow"
+						content={(
+							<React.Fragment>
+								<div className="text">Proceed</div>
+								<Icon className="arrow-right" />
+							</React.Fragment>
+						)}
+					/>
+				</div>
+			</div>
+		);
+	}
+
+}
+
+export default SelectLanguage;
--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 
-import { AUTHORIZATION, ACCOUNT_CREATED } from './constants/routes';
+import {
+	AUTHORIZATION,
+	ACCOUNT_CREATED,
+	SELECT_LANGUAGE,
+} from './constants/routes';
+
 import App from './containers/app';
 import Authorization from './containers/authorization';
 import accountCreated from './containers/account-сreated';
+import selectLanguage from './containers/select-language';
+
 
 export default () => (
 	<App>
 		<Switch>
 			<Route path={AUTHORIZATION} component={Authorization} />
 			<Route path={ACCOUNT_CREATED} component={accountCreated} />
+			<Route path={SELECT_LANGUAGE} component={selectLanguage} />
 		</Switch>
 	</App>
 );
