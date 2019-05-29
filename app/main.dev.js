1cc2ccd6e8a17cd3b59897c7868b0bdb8e56a17e
Alina Lozyuk <a.lozyuk@pixelplex.io>
2019-05-29 19:18:45 +0300

    [BLP-134] fix white screen

--- app/main.dev.js
+++ app/main.dev.js
@@ -185,6 +185,7 @@ app.on('ready', async () => {
 		height: APP_WINDOW_HEIGHT,
 		minWidth: APP_WINDOW_MIN_WIDTH,
 		minHeight: APP_WINDOW_MIN_HEIGHT,
+		backgroundColor: '#6852A4',
 		frame: false,
 	});
 
@@ -244,7 +245,6 @@ app.on('ready', async () => {
 		if (process.env.START_MINIMIZED) {
 			mainWindow.minimize();
 		} else {
-			mainWindow.show();
 			mainWindow.focus();
 		}
 
@@ -373,6 +373,10 @@ ipcMain.on('close-app', (event) => {
 
 	return false;
 	// app.quit();
+});
+
+ipcMain.on('showWindow', () => {
+	mainWindow.show();
 });
 
 ipcMain.on('zoom-app', () => {
