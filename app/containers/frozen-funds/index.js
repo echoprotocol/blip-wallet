cb3661ab058787dcfab4ca976f8f5a58f170f9ad
kosAndre22 <a.kosobuckiy@pixelplex.io>
2019-11-01 13:51:16 +0300

    remove commits

--- app/containers/frozen-funds/index.js
+++ app/containers/frozen-funds/index.js
@@ -2,15 +2,6 @@ import { connect } from 'react-redux';
 import FrozenFunds from '../../components/frozen-funds';
 import { getFrozenBalance } from '../../actions/balance-actions';
 
-// export default connect(
-// 	(state) => ({
-// 		frozenBalances: state.wallet.get('frozenBalances'),
-// 	}),
-
-// 	(dispatch) => ({
-// 		getFrozenBalance: () => dispatch(getFrozenBalance()),
-// 	}),
-// )(FrozenFunds);
 export default connect(
 	(state) => ({
 		frozenBalances: state.wallet.get('frozenBalances'),
