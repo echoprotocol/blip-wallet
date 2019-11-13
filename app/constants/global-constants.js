681cb97be86c05c0e6ffdf9efce43a7870f2ddd0
maxshev <31995360+maxshev@users.noreply.github.com>
2019-11-13 15:39:43 +0300

    [BLP-479] Added hover effect in the side menu & edited texts for freeze form (#50)
    * [BLP-468] Fixed font-weight at Frozen form
    
    * [BLP-469] Added style for disabled inputs
    
    * [BLP-473] Fixed design at frozen form
    
    * [BLP-473] Fixed button
    
    * [BLP-473] Removed
    
    * [BLP-475] Fixed button
    
    * [BLP-478] Fixed avatar size
    
    * [BLP-480] Fixed fonts color in table
    
    * [BLP-479] Added hover effect at side-menu
    
    * Disable logic for account input & placeholders for rus.lang
    
    * Added global constans for dropdown & accounts count
    
    * Fixed lint issue

--- app/constants/global-constants.js
+++ app/constants/global-constants.js
@@ -41,6 +41,8 @@ export const RANDOM_SIZE = 2048;
 export const NAME_MIN_LENGTH = 1;
 export const NAME_MAX_LENGTH = 63;
 
+export const MIN_DROPDOWN_ITEMS_COUNT = 1;
+
 export const CONNECTION_TIMEOUT = 5000;
 export const MAX_RETRIES = 999999999;
 export const PING_TIMEOUT = 7000;
